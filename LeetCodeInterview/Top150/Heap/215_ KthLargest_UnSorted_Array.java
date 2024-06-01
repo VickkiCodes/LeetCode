561bfe7985556cdfc0a90666cad3c15d4ca3e5b9
cryptonkid <cryptonkid@gmail.com>
2024-06-01 11:37:33 +0200

    1/6/24:Top150:Heap:Medium : Enhanced Approach

--- LeetCodeInterview/Top150/Heap/215_ KthLargest_UnSorted_Array.java
+++ LeetCodeInterview/Top150/Heap/215_ KthLargest_UnSorted_Array.java
@@ -4,6 +4,14 @@ class Solution {
         PriorityQueue<Integer> input = new PriorityQueue<>();
         for(int i=0;i<nums.length;i++){
             input.offer(nums[i]*-1);
+            /**
+             * Imporved Approach:
+             *  Storing only 'K heap numbers' only.
+             *
+                  if (heap.size() > k) {
+                      heap.remove();
+                   }
+             */
         }
         for(int i=0;i<k-1;i++){
             input.poll();
